787653e3e9ee3afe469bac4ea312e7e9390a49b0
Ridwan-AI <ridwan@rnetworks.cf>
2021-04-02 11:07:53 +0600

    Discounting added

--- task1.py
+++ task1.py
@@ -1,7 +1,7 @@
-# Task 1
-# Start of the day
+################################################# Task 1 #################################################
+############################################# Start of the day ###########################################
 # Coded by Ridwan Arefin Islam, Need help anywhere? Go to https://caie.rnetworks.cf/
-#This is a test2.
+
 print ("\nGood morning\n")
 
 #Initialise structure data for passengers
@@ -19,12 +19,10 @@ print ("Time: 09:00  Tickets remaining: " + str(passenger_left_departure[0]) + "
 print ("Time: 11:00  Tickets remaining: " + str(passenger_left_departure[1]) + "  Money collected: " + str(totalmoney_departure[1]) + "  Price: $25/ticket" + "           Time: 12:00  Tickets remaining: " + str(passenger_left_return[1]) + "  Money collected: " + str(totalmoney_return[1]) + "  Price: $25/ticket")
 print ("Time: 13:00  Tickets remaining: " + str(passenger_left_departure[2]) + "  Money collected: " + str(totalmoney_departure[2]) + "  Price: $25/ticket" + "           Time: 14:00  Tickets remaining: " + str(passenger_left_return[2]) + "  Money collected: " + str(totalmoney_return[2]) + "  Price: $25/ticket")
 print ("Time: 15:00  Tickets remaining: " + str(passenger_left_departure[3]) + "  Money collected: " + str(totalmoney_departure[3]) + "  Price: $25/ticket" + "           Time: 16:00  Tickets remaining: " + str(passenger_left_return[3]) + "  Money collected: " + str(totalmoney_return[3]) + "  Price: $25/ticket\033[0m")
-
 print("")
-# Task 2
+################################################# Task 2 #################################################
+############################################## Purchasing tickets ########################################
 
-# Purchasing tickets
-# print("\033[1;32;40mBright Green\033[0m\n")
 wants = "abc"
 numoftickets = 0
 validcheck = True
@@ -57,6 +55,15 @@ while (validcheck == True):
                 # wantsdeparttime = wantsdeparttime.strip()
                 #Validate that tickets are available for departure
                 purchasedticket = False
+
+                #First letter of numoftickets
+                if (numoftickets<=80 and numoftickets>=10):
+                    discout = str(numoftickets) #Notice the spellings of discout and discount
+                    discount = int(discout[0])
+                    print (discount, "of your tickets will be discounted.")
+                else:
+                    discount = 0
+                
                 while (purchasedticket == False):
                     wantsdeparttime = input("When do you want to go for departure to the top of the mountain?\n")
                     wantsdeparttime = wantsdeparttime.strip()
@@ -66,7 +73,7 @@ while (validcheck == True):
                             purchasesuccess = True
                             purchasedticket = True
                             passenger_left_departure[0] = passenger_left_departure[0] - numoftickets
-                            totalmoney_departure[0] = totalmoney_departure[0] + numoftickets*25
+                            totalmoney_departure[0] = totalmoney_departure[0] + (numoftickets-discount)*25
                             if (passenger_left_departure[0] == 0):
                                 passenger_left_departure[0] = "Closed"
                     elif (wantsdeparttime == "11:00" and passenger_left_departure[1] !="Closed"):
@@ -75,7 +82,7 @@ while (validcheck == True):
                             purchasesuccess = True
                             purchasedticket = True
                             passenger_left_departure[1] = passenger_left_departure[1] - numoftickets
-                            totalmoney_departure[1] = totalmoney_departure[1] + numoftickets*25
+                            totalmoney_departure[1] = totalmoney_departure[1] + (numoftickets-discount)*25
                             if (passenger_left_departure[1] == 0):
                                 passenger_left_departure[1] = "Closed"
                     elif (wantsdeparttime == "13:00" and passenger_left_departure[2] != "Closed"):
@@ -84,7 +91,7 @@ while (validcheck == True):
                             purchasesuccess = True
                             purchasedticket = True
                             passenger_left_departure[2] = passenger_left_departure[2] - numoftickets
-                            totalmoney_departure[2] = totalmoney_departure[2] + numoftickets*25
+                            totalmoney_departure[2] = totalmoney_departure[2] + (numoftickets-discount)*25
                             if (passenger_left_departure[2] == 0):
                                 passenger_left_departure[2] = "Closed"
                     elif (wantsdeparttime == "15:00" and passenger_left_departure[3] != "Closed"):
@@ -93,7 +100,7 @@ while (validcheck == True):
                             purchasesuccess = True
                             purchasedticket = True
                             passenger_left_departure[3] = passenger_left_departure[3] - numoftickets
-                            totalmoney_departure[3] = totalmoney_departure[3] + numoftickets*25
+                            totalmoney_departure[3] = totalmoney_departure[3] + (numoftickets-discount)*25
                             if (passenger_left_departure[3] == 0):
                                 passenger_left_departure[3] = "Closed"
                     else:
@@ -110,7 +117,7 @@ while (validcheck == True):
                             purchasesuccess = True
                             purchasedticket = True
                             passenger_left_return[0] = passenger_left_return[0] - numoftickets
-                            totalmoney_return[0] = totalmoney_return[0] + numoftickets*25
+                            totalmoney_return[0] = totalmoney_return[0] + (numoftickets-discount)*25
                             if (passenger_left_return[0] == 0):
                                 passenger_left_return[0] = "Closed"
                     elif (wantsreturntime == "12:00" and passenger_left_return[1] != "Closed" and wantsdeparttime != "13:00" and wantsdeparttime != "15:00"):
@@ -119,7 +126,7 @@ while (validcheck == True):
                             purchasesuccess = True
                             purchasedticket = True
                             passenger_left_return[1] = passenger_left_return[1] - numoftickets
-                            totalmoney_return[1] = totalmoney_return[1] + numoftickets*25
+                            totalmoney_return[1] = totalmoney_return[1] + (numoftickets-discount)*25
                             if (passenger_left_return[1] == 0):
                                 passenger_left_return[1] = "Closed"
                     elif (wantsreturntime == "14:00" and passenger_left_return[2] != "Closed" and wantsdeparttime != "15:00"):
@@ -128,7 +135,7 @@ while (validcheck == True):
                             purchasesuccess = True
                             purchasedticket = True
                             passenger_left_return[2] = passenger_left_return[2] - numoftickets
-                            totalmoney_return[2] = totalmoney_return[2] + numoftickets*25
+                            totalmoney_return[2] = totalmoney_return[2] + (numoftickets-discount)*25
                             if (passenger_left_return[2] == 0):
                                 passenger_left_return[2] = "Closed"
                     elif (wantsreturntime == "16:00" and passenger_left_return[3] != "Closed"):
@@ -137,7 +144,7 @@ while (validcheck == True):
                             purchasesuccess = True
                             purchasedticket = True
                             passenger_left_return[3] = passenger_left_return[3] - numoftickets
-                            totalmoney_return[3] = totalmoney_return[3] + numoftickets*25
+                            totalmoney_return[3] = totalmoney_return[3] + (numoftickets-discount)*25
                             if (passenger_left_return[3] == 0):
                                 passenger_left_return[3] = "Closed"
                     else:
@@ -191,7 +198,7 @@ while (validcheck == True):
                             if (passenger_left_departure[3] == 0):
                                 passenger_left_departure[3] = "Closed"
                     else:
-                        print ("\033[1;31;40mDeparture ticket purchase failed, either due to one of the reasons below:\n•Not enough tickets available for that time slot\n•You entered such a time slot that does not exist\033[0m")
+                        print ("\033[1;31;40mDeparture ticket purchase failed, either due to one of the reasons below:\n•Not enough tickets available for that time slot\n•You entered such a time slot that does not exist\033[0m\n")
                 #Return time
                 #Validate that tickets are available for return
                 purchasedticket = False
@@ -235,7 +242,7 @@ while (validcheck == True):
                             if (passenger_left_return[3] == 0):
                                 passenger_left_return[3] = "Closed"
                     else:
-                        print ("\033[1;31;40mDeparture ticket purchase failed, either due to one of the reasons below:\n•Not enough tickets available for that time slot\n•You entered such a time slot that does not exist\n•Chosen return time is before the departure time, which is impossible\n\033[0m")
+                        print ("\033[1;31;40mDeparture ticket purchase failed, either due to one of the reasons below:\n•Not enough tickets available for that time slot\n•You entered such a time slot that does not exist\n•Chosen return time is before the departure time, which is impossible\033[0m\n")
             
             else:
                 #Validation failed
@@ -259,7 +266,16 @@ while (validcheck == True):
         print ("Time: 13:00  Tickets remaining: " + str(passenger_left_departure[2]) + "  Money collected: " + str(totalmoney_departure[2]) + "  Price: $25/ticket" + "           Time: 14:00  Tickets remaining: " + str(passenger_left_return[2]) + "  Money collected: " + str(totalmoney_return[2]) + "  Price: $25/ticket")
         print ("Time: 15:00  Tickets remaining: " + str(passenger_left_departure[3]) + "  Money collected: " + str(totalmoney_departure[3]) + "  Price: $25/ticket" + "           Time: 16:00  Tickets remaining: " + str(passenger_left_return[3]) + "  Money collected: " + str(totalmoney_return[3]) + "  Price: $25/ticket\033[0m")
 
+################################################# Task 3 #################################################
+############################################# End of the day #############################################
 
+print ("We have reached the end of the day! So down below are the total money taken and the passengers travelled on each train journey.")
+
+print ("\033[1;34;40mDeparture to top of mountain                                                         Return from top of mountain")
+print ("Time: 09:00  Passengers travelled: " + str(480-passenger_left_departure[0]) + "  Money collected: " + str(totalmoney_departure[0]) + "  Price: $25/ticket" + "           Time: 10:00  Passengers travelled: " + str(480-passenger_left_return[0]) + "  Money collected: " + str(totalmoney_return[0]) + "  Price: $25/ticket")
+print ("Time: 11:00  Passengers travelled: " + str(480-passenger_left_departure[1]) + "  Money collected: " + str(totalmoney_departure[1]) + "  Price: $25/ticket" + "           Time: 12:00  Passengers travelled: " + str(480-passenger_left_return[1]) + "  Money collected: " + str(totalmoney_return[1]) + "  Price: $25/ticket")
+print ("Time: 13:00  Passengers travelled: " + str(480-passenger_left_departure[2]) + "  Money collected: " + str(totalmoney_departure[2]) + "  Price: $25/ticket" + "           Time: 14:00  Passengers travelled: " + str(480-passenger_left_return[2]) + "  Money collected: " + str(totalmoney_return[2]) + "  Price: $25/ticket")
+print ("Time: 15:00  Passengers travelled: " + str(480-passenger_left_departure[3]) + "  Money collected: " + str(totalmoney_departure[3]) + "  Price: $25/ticket" + "           Time: 16:00  Passengers travelled: " + str(480-passenger_left_return[3]) + "  Money collected: " + str(totalmoney_return[3]) + "  Price: $25/ticket\033[0m")
 
 
 #Just a newline to make output look nicer
